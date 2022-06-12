247e86a842007146ed48d9d3265fd708c09bfc66
Shashank <langishashank@gmail.com>
2022-06-12 12:03:43 +0530

    [Shashank]:Creating a Table.

--- EmpPayRoll.sql
+++ EmpPayRoll.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE Emp_Pay_Roll
 
 USE Emp_Pay_Roll
+
+CREATE TABLE Employee_Pay_Roll
+(
+	ID int identity (1,1) primary key,
+	Name varchar (30) NOT NULL,
+	salary int,
+	StartDate Date
+);
