7d06178e76c8932442f6c24e7ae7afe013680b2f
Shashank <langishashank@gmail.com>
2022-06-12 12:23:13 +0530

    [Shashank]:Retrieving Data from the table.

--- EmpPayRoll.sql
+++ EmpPayRoll.sql
@@ -14,3 +14,5 @@ INSERT INTO Employee_Pay_Roll VALUES('Jonny', 7500, '2021-10-03')
 INSERT INTO Employee_Pay_Roll VALUES('Jaden', 5500, '2022-02-22')
 INSERT INTO Employee_Pay_Roll VALUES('Captain', 2000, '2021-12-10')
 INSERT INTO Employee_Pay_Roll VALUES('Hulk', 9600, '2022-01-15')
+
+SELECT * FROM Employee_Pay_Roll
